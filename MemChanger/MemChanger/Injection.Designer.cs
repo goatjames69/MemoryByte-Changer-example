f4d9ce0ccfac7771073aebc357835102794abe6b
imjamesxd <129680954+jamesxdstore@users.noreply.github.com>
2023-05-05 16:32:26 +0545

    Add files via upload

---
+++ MemChanger/MemChanger/Injection.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace MemChanger
+{
+    partial class Injection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siticoneButton1 = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.s = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.r = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.siticoneButton1.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.Location = new System.Drawing.Point(263, 224);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton1.TabIndex = 0;
+            this.siticoneButton1.Text = "Inject";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // s
+            // 
+            this.s.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.s.BorderRadius = 5;
+            this.s.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.s.DefaultText = "";
+            this.s.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.s.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.s.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.s.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.s.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.s.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.s.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.s.ForeColor = System.Drawing.Color.White;
+            this.s.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.s.Location = new System.Drawing.Point(80, 87);
+            this.s.Name = "s";
+            this.s.PasswordChar = '\0';
+            this.s.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.s.PlaceholderText = "Scan:";
+            this.s.SelectedText = "";
+            this.s.Size = new System.Drawing.Size(553, 36);
+            this.s.TabIndex = 1;
+            // 
+            // r
+            // 
+            this.r.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.r.BorderRadius = 5;
+            this.r.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.r.DefaultText = "";
+            this.r.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.r.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.r.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.r.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.r.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.r.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.r.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.r.ForeColor = System.Drawing.Color.White;
+            this.r.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.r.Location = new System.Drawing.Point(80, 158);
+            this.r.Name = "r";
+            this.r.PasswordChar = '\0';
+            this.r.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.r.PlaceholderText = "Replace:";
+            this.r.SelectedText = "";
+            this.r.Size = new System.Drawing.Size(553, 36);
+            this.r.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status";
+            // 
+            // Injection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(13)))), ((int)(((byte)(13)))));
+            this.ClientSize = new System.Drawing.Size(707, 344);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.r);
+            this.Controls.Add(this.s);
+            this.Controls.Add(this.siticoneButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Injection";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MEMORY";
+            this.Load += new System.EventHandler(this.Injection_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox s;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox r;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MemChanger/MemChanger/Injection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MemChanger;
+using Memory;
+//string by james
+namespace MemChanger
+{
+    public partial class Injection : Form
+    {
+        public Injection()
+        {
+            InitializeComponent();
+        }
+        Mem pnlmem = new Mem();
+        private object name;
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void aasxx();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+         (
+             int nLeftRect,
+             int nTopRect,
+             int nRightRect,
+             int nBottomRect,
+             int nWidthEllipse,
+             int nHeightEllipse
+         );
+        private void Injection_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Injecting your Function!";
+            int pnllist = pnlmem.GetProcIdFromName("HD-Player.exe");
+            if (pnllist > 0)
+            {
+                pnlmem.OpenProcess(pnllist);
+                IEnumerable<long> AoBScanResults = await pnlmem.AoBScan(0L, 140737488355327L, s.Text, true, true);
+                if (AoBScanResults.Count() == 0)
+                {
+                }
+                else if (AoBScanResults.Count() > 0)
+                {
+                    for (int i = 0; i < AoBScanResults.Count(); i++)
+                        pnlmem.WriteMemory(AoBScanResults.ElementAt(i).ToString("X"), "bytes", r.Text, "", (Encoding)null);
+                    label1.Text = "Injected!";
+                }
+            }
+        }
+    }
+}
+//string by james
